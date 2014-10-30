14b8988b26b0bf397e6ccdac95187d88510422d3
Strannik-j <strannik-j@mail.ru>
2014-10-30 11:04:33 +0300

    Add sha512 verification and move backups to remote server. Other fiches and fix.

--- backup_site.sh
+++ backup_site.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 SITE_DIR='/home/www-data/www/example.com'
-BACK_DIR='/home/www-data/backups/example.com'
+BACK_DIR='/home/www-data/$R_USER/example.com'
 DB_NAME='example'
 DB_USER='root'
 DB_PASS='123456789'
 DATE_NOW=`date +%Y-%m-%d.%H_%M_%S`
 
-#Move backups to another server? (TRUE/FALSE)
+#Move $R_USER to remote server? (TRUE/FALSE)
 MOVE_SCP='FALSE'
 
 #Archivators configs
@@ -16,9 +16,16 @@ A_PGZIP='pbzip -k -p'$CORES' -c '
 A_BZIP2='bzip2 -c' 
 A_GZIP='gzip -c'
 
-#############
-#DO NOT EDIT#
-#############
+#Remote server config
+#Connetion over SCP with RSA key or password
+R_HOST='192.168.0.1'
+R_PORT=22
+R_USER='backups'
+R_PATH='/home/backups/archives'
+
+###############
+# DO NOT EDIT #
+###############
 if (test -e /usr/bin/pbzip2);then ARCHIVATOR=$A_PBZIP2 EXTRACTOR='bzcat' SUF='bz';\
 elif (test -e /usr/bin/pgzip);then ARCHIVATOR=$A_PGZIP EXTRACTOR='gzcat' SUF='gz';\
 elif (test -e /bin/bzip2);then ARCHIVATOR=$A_BZIP2 EXTRACTOR='bzcat' SUF='bz';\
@@ -34,19 +41,24 @@ time mysqldump -u $DB_USER -p$DB_PASS $DB_NAME | $ARCHIVATOR > $BACK_DIR'/dump.s
 echo "**************"
 echo "Archives size:"
 echo ""
-ls -l -h $BACK_DIR'/dump.sql.'$DATE_NOW'.'$SUF | cut --bytes=31- -
-ls -l -h $BACK_DIR'/site.back.'$DATE_NOW'.t'$SUF | cut --bytes=31- -
-
-##############
-#RESTORE FILE#
-##############
+ls -l -h $BACK_DIR/dump.sql.$DATE_NOW.$SUF | cut --bytes=31- -
+ls -l -h $BACK_DIR/site.back.$DATE_NOW.t$SUF | cut --bytes=31- -
+sha512sum $BACK_DIR/site.back.$DATE_NOW.t$SUF $BACK_DIR/dump.sql.$DATE_NOW.$SUF > $BACK_DIR/sha512_$DATE_NOW.txt
+if [ $MOVE_SCP = 'TRUE' ] ; then scp -P $R_PORT $BACK_DIR/dump.sql.$DATE_NOW.$SUF $BACK_DIR/site.back.$DATE_NOW.t$SUF $R_USER@$R_HOST:$R_PATH/ && rm $BACK_DIR/dump.sql.$DATE_NOW.$SUF $BACK_DIR/site.back.$DATE_NOW.t$SUF && echo "Archives moved" ; fi
+################
+# RESTORE FILE #
+################
 echo "#!/bin/bash" > $BACK_DIR/restore.$DATE_NOW.sh
 echo "# RESTORE SITE AND DB" >> $BACK_DIR/restore.$DATE_NOW.sh
 if [ $MOVE_SCP = "TRUE" ] ; then \
-echo "" >> $BACK_DIR/restore.$DATE_NOW.sh;
-else echo "tar -P --"$SUF" -xvf "$BACK_DIR"/site.back."$DATE_NOW".t"$SUF >> $BACK_DIR/restore.$DATE_NOW.sh;
+echo "scp -P $R_PORT $R_USER@$R_HOST:$R_PATH/dump.sql.$DATE_NOW.$SUF $BACK_DIR/" >> $BACK_DIR/restore.$DATE_NOW.sh;
+echo "scp -P $R_PORT $R_USER@$R_HOST:$R_PATH/site.back.$DATE_NOW.t$SUF $BACK_DIR/" >> $BACK_DIR/restore.$DATE_NOW.sh;
+else echo "";
 fi
-echo $SUF"cat "$BACK_DIR"/dump.sql."$DATE_NOW"."$SUF" | mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME >> $BACK_DIR/restore.$DATE_NOW.sh;
+echo "sha512sum -c $BACK_DIR/sha512_$DATE_NOW.txt || echo 'Archives is NOT correct. STOP' exit 0" >> $BACK_DIR/restore.$DATE_NOW.sh;
+echo "echo 'Archives is correct. Start restore'" >> $BACK_DIR/restore.$DATE_NOW.sh;
+echo "tar -P --"$SUF" -xf "$BACK_DIR"/site.back."$DATE_NOW".t"$SUF" && echo 'SITE archive extracted'" >> $BACK_DIR/restore.$DATE_NOW.sh;
+echo $SUF"cat "$BACK_DIR"/dump.sql."$DATE_NOW"."$SUF" | mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME"&& echo 'DB archive extracted' && rm cat "$BACK_DIR"/dump.sql."$DATE_NOW"."$SUF $BACK_DIR"/site.back."$DATE_NOW".t"$SUF >> $BACK_DIR/restore.$DATE_NOW.sh;
 chmod +x $BACK_DIR/restore.$DATE_NOW.sh
 
 exit 0
